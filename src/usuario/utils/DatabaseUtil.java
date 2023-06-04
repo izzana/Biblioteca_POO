c5203d058e730413dd139d82fbcddf2e5cc313cf
izzana <izzana.martins@gmail.com>
2023-06-03 23:17:32 -0300

    feat: created usuarios table

--- src/usuario/utils/DatabaseUtil.java
+++ src/usuario/utils/DatabaseUtil.java
@@ -20,19 +20,21 @@ public class DatabaseUtil {
     private Connection conn;
     private Statement stmt;//possui métodos para fazermos manipulações no BD, como SELECT, CREATETABLE
     
-    public void createContactsDatabase() {
+    public void createUsuarioTable() {
         try {
             Class.forName("org.hsqldb.jdbc.JDBCDriver"); //registrando no projeto onde está a o driver de conexão com o sqldb 
-            this.conn = DriverManager.getConnection("jdbc:hsqldb:file:./database/dbagenda");
+            this.conn = DriverManager.getConnection("jdbc:hsqldb:file:./database/db_biblioteca");
             this.stmt = this.conn.createStatement(); 
-            this.stmt.executeUpdate("CREATE TABLE contatos (id IDENTITY PRIMARY KEY,"+ //executeUpdate -> executa comandos no BD
-                                    "cpf VARCHAR(11)," +
+            this.stmt.executeUpdate("CREATE TABLE usuario (id IDENTITY PRIMARY KEY,"+ //executeUpdate -> executa comandos no BD
                                     "nome VARCHAR(50)," +
+                                    "cpf VARCHAR(11)," +
                                     "email VARCHAR(50)," +
-                                    "telefone VARCHAR(50));");
+                                    "telefone VARCHAR(14)," +
+                                    "login VARCHAR(50)," +
+                                    " senha VARCHAR(50));");
             this.stmt.close();
             this.conn.close();
-            System.out.println("Tabela contatos criada com sucesso!");
+            System.out.println("Tabela usuarios criada com sucesso!");
         } catch (ClassNotFoundException ex) {
             System.out.println("Classe não encontrada");
             Logger.getLogger(DatabaseUtil.class.getName()).log(Level.SEVERE, null, ex);
@@ -43,7 +45,7 @@ public class DatabaseUtil {
     
     public static void main(String args[]) {
         DatabaseUtil util = new DatabaseUtil();
-        util.createContactsDatabase();
+        util.createUsuarioTable();
     }
 }
 
